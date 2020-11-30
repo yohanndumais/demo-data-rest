b7712ca7b8e524e8fa2bc1de90897f1afef23aa7
Yohann Dumais <yohann.dumais@dti.ulaval.ca>
2020-11-30 11:05:05 -0500

    Initial commit

---
+++ src/main/java/com/example/demodatarest/DemoDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDataRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demodatarest/DemoDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
